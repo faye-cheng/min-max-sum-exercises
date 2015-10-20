75d87295f36aef1785a61dfd465db084490697c3
Faye Cheng <fayehcheng@gmail.com>
2015-10-20 10:35:14 -0700

    Create ClearBHandler.java

---
+++ ClearBHandler.java
@@ -0,0 +1,14 @@
+package forEx8_17;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class ClearBHandler implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
